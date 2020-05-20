d7a692ee3618de550f994ca4ef0bfd5e4146f292
Drew Derbyshire <ahd@kew.com>
2020-05-20 11:02:24 -0700

    Support only examining device reported by Hercules

--- bin/hercules_route_lcs.py
+++ bin/hercules_route_lcs.py
@@ -4,6 +4,7 @@
 
 """Add a subnetwork IP address to the correct TAP adapter for a VM."""
 
+import argparse
 import errno
 import re
 import subprocess
@@ -11,9 +12,36 @@ import sys
 
 __author__ = "ahd@kew.com (Drew Derbyshire)"
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
-def _ReadOatFile(filepath):
+_PARSER = argparse.ArgumentParser(
+    description='Generate IP address to route Hercules tap device',
+    add_help=True)
+
+
+def _BuildParser():
+  """Add command arguments to the command line parser."""
+  _PARSER.add_argument('--version',
+                       '-v',
+                       help='Print the program version',
+                       action='version',
+                       version='%(prog)s by {} version {}'.format(
+                           __author__, __version__))
+  _PARSER.add_argument('--device',
+                       '-d',
+                       help='tap device to check and if void of an IP'
+                       ' address and if none found, add one.',
+                       default=argparse.SUPPRESS,
+                       type=str)
+  _PARSER.add_argument('oat',
+                       default=argparse.SUPPRESS,
+                       nargs='+',
+                       type=argparse.FileType('r'),
+                       help='OAT file to read for MAC and IP addresses'
+                       ' of the Hercules device')
+
+
+def _ReadOatFile(handle):
   """The OAT file for the LCS Ethernet and network addresses"""
   mac_address = None
   machine_ip = None
@@ -23,20 +51,19 @@ def _ReadOatFile(filepath):
   regex_mac = re.compile(
       r'\s*HWADD\s+00\s+(([\dA-Fa-f]{2,2}[-:]){5}[\dA-Fa-f]{2,2})\s*')
 
-  with open(filepath) as handle:
-    for data in handle:
-      data = data.rstrip()
-      print(data, end='\t')
-      match = regex_ip.match(data)
+  for data in handle:
+    data = data.rstrip()
+    print(data, end='\t')
+    match = regex_ip.match(data)
+    if match:
+      machine_ip = match.group(1)
+      print('IP =', machine_ip, end='\t')
+    else:
+      match = regex_mac.match(data)
       if match:
-        machine_ip = match.group(1)
-        print('IP =', machine_ip, end='\t')
-      else:
-        match = regex_mac.match(data)
-        if match:
-          mac_address = match.group(1).replace('-', ':').lower()
-          print('MAC =', mac_address, end='\t')
-      print()
+        mac_address = match.group(1).replace('-', ':').lower()
+        print('MAC =', mac_address, end='\t')
+    print()
 
   if not mac_address:
     print('No MAC address found, exiting')
@@ -49,11 +76,10 @@ def _ReadOatFile(filepath):
 
 
 def _ReadOneInterface(entry, mac, gateway_ip, device):
-  """Parse 'ip addr list' :w
-  entry looking for our target device"""
+  """Parse 'ip addr list' entry looking for our target device"""
 
   if not device.startswith('tap'):
-    print('Device', device, 'is not a tap device.')
+    # device is not a tap device.
     return (None, None)
 
   this_mac = None
@@ -94,12 +120,13 @@ def _ReadOneInterface(entry, mac, gateway_ip, device):
   return (None, device)   # Possible fallback device
 
 
-def _ReadInterfaces(mac, gateway_ip):
+def _ReadInterfaces(mac, gateway_ip, device):
   """Find the unconfigured interface with the specified Mac address"""
   regex_entry = re.compile(r'^\d+:', flags=(re.DOTALL|re.MULTILINE))
   regex_device = re.compile(r'\s*([a-zA-Z]{2,4}\d*): <.+>.+')
 
-  proc = subprocess.run(('ip', 'addr', 'list'),
+  args = ('ip addr list ' + (device or '')).split()
+  proc = subprocess.run(args,
                         capture_output=True,
                         check=True,
                         text=True)
@@ -135,9 +162,9 @@ def _ReadInterfaces(mac, gateway_ip):
   return fallback_return
 
 
-def _ProcessOne(system_name):
+def _ProcessOne(handle, device):
   """Parse and route one system."""
-  mac_address, machine_ip = _ReadOatFile(system_name + ".oat")
+  mac_address, machine_ip = _ReadOatFile(handle)
   print(mac_address, machine_ip)
 
   token = machine_ip.split('.')
@@ -147,11 +174,12 @@ def _ProcessOne(system_name):
     token[3] = '1'
   gateway_ip = '.'.join(token)
 
-  device = _ReadInterfaces(mac_address, gateway_ip)
+  device = _ReadInterfaces(mac_address, gateway_ip, device)
 
   args = 'sudo ip addr add xxx.xxxx.xxx.xxx/24 dev xxxx'.split()
   args[-3] = gateway_ip + '/24'
   args[-1] = device
+  print('Executing:', ' '.join(args))
 
   subprocess.run(args,
                  stdout=sys.stdout,
@@ -161,8 +189,14 @@ def _ProcessOne(system_name):
 
 def _Main():
   """Main progress to process all systems on command line."""
-  for system in sys.argv[1:]:
-    _ProcessOne(system)
+  print(sys.argv[0], __version__)
+  _BuildParser()
+  cli_flags = vars(_PARSER.parse_args())
+  for system in cli_flags['oat']:
+    if 'device' in cli_flags:
+      _ProcessOne(system, cli_flags['device'])
+    else:
+      _ProcessOne(system, None)
   sys.exit(0)
 
 if __name__ == '__main__':
