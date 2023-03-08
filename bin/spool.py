39892c9a9f3f03aed551285101e0e9ae5fa13e62
Drew Derbyshire <ahd@kew.com>
2023-03-08 09:14:58 -0800

    Support banner pages generated by SYSY0994 under MVT

--- bin/spool.py
+++ bin/spool.py
@@ -21,8 +21,9 @@ the number of separator page lines to 1:
 """
 
 __author__ = "ahd@kew.com (Drew Derbyshire)"
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 
+from datetime import datetime
 import os
 import re
 import sys
@@ -40,13 +41,13 @@ _JES2_PATTERN = (
     r'  (?P<edge>START| END )'              # START/END
     r'  (?P<queue>JOB|STC|TSU)'             # JOB/TSU/STC
     r' (?P<number>[ \d]{4,4})'              # job number
-    r'  (?P<name>[A-Z0-9@#$ ]{8,8})'        # job name
+    r'  (?P<jobname>[A-Z0-9@#$ ]{8,8})'     # job name
     r'  (.{20,20})'                         # programmer name
     r'  ROOM (?P<room>[\w ]{4,4})'          # room number
     r'  [ \d]\d(?:\.\d\d){2,2} (?:A|P)M'    # time: hh:mm:ss AM/PM
     r' \d\d [A-Z]{3,3} \d\d'                # date: dd mmm yy
     r'  ([\w ]{8,8})'                       # printer name
-    r'  SYS (?P<system>[\w ]{4,4})'         # system name
+    r'  SYS (?P<smf>[\w ]{4,4})'            # system name
     r'  (?P=queue)'                         # JOB/TSU/STC
     r' (?P=number)'                         # job number
     r'  (?P=edge)'                          # START/END
@@ -76,7 +77,7 @@ _HASP_PATTERN = (
     r'\.{4,4}'                              # Literal
     r'ROOM (?P<room>[\w ]{4,4})'            # room number
     r'\.{4,4}'                              # Literal
-    r'(?P<name>[A-Z0-9@#$ ]{8,8})'          # job name
+    r'(?P<jobname>[A-Z0-9@#$ ]{8,8})'       # job name
     r'\.{4,4}'                              # Literal
     r'(?P<pgmr>.{20,20})'                   # programmer name
     r'\.{4,4}'                              # Literal
@@ -86,6 +87,42 @@ _HASP_PATTERN = (
     r'\*HASP-II'                            # ****
 )
 _HASP_REGEX = re.compile(_HASP_PATTERN)
+
+# pylint: disable=C0301
+#       ....+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8....+....9....+....*....+....1....+....2....+....3..
+#       ***********A  START   G02VSCPY   SYS SYSA   WTR 00E   07:38:43 PM  28 AUG 1978   WTR 00E   SYS SYSA   G02VSCPY   START  A***********
+#       ....+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8....+....9....+....*....+....1....+....2....+....3..
+
+_WTR_PATTERN = (
+    r'\*{11,11}'                            # Literal
+    r'(?P<class>[A-Z0-9])'                  # sysout class
+    r'  '                                   # Literal
+    r'(?P<edge>START|\.\.END)'              # START/END
+    r'   '                                  # Literal
+    r'(?P<jobname>[A-Z0-9@#$ ]{8,8})'       # job name
+    r'   SYS '                              # Literal
+    r'(?P<smf>[ \w]{4,4})'                  # system SMF id
+    r'   '                                  # Literal
+    r'(?P<queue>WTR|JOB|STC|TSU)'           # WTR JOB/TSU/STC
+    r' (?P<printer>[\w ]{3,3})'             # printer address (name)
+    r'   '                                  # Literal
+    r'[ \d]\d(:\d\d){2,2} (?:A|P)M'      # time: hh:mm:ss AM/PM
+    r'  '                                   # Literal
+    r'[ \d]\d [A-Z]{3,3} \d{4,4}'           # date: dd mmm yyyy
+    r'   '                                  # Literal
+    r'(?P=queue)'                           # WTR
+    r' (?P=printer)'                        # printer address (name)
+    r'   SYS '                              # Literal
+    r'(?P=smf)'                             # system SMF id
+    r'   '                                  # Literal
+    r'(?P=jobname)'                        # job name
+    r'   '                                  # Literal
+    r'(?P=edge)'                           # START/END
+    r'  '                                   # Literal
+    r'(?P=class)'                          # sysout class
+    r'\*{11,11}'                            # Literal
+)
+_WTR_REGEX = re.compile(_WTR_PATTERN)
 
 def _GetLine():
   """Read one line of sysout from the input"""
@@ -106,76 +143,103 @@ def _GetLine():
     if c in ('\n', '\f'):
       return line
 
+def _OpenFile(dictionary):
+  """Open a new spool based on provided job information."""
+  if not dictionary:
+    # Fake job information if none provided
+    current = datetime.now()
+    dictionary = {
+        'class': 'Q',
+        'jobname': ''.join(('X',
+                            current.strftime('%j%H%M'))),
+        'number': current.strftime('%S'),
+        'smf': 'SPL',
+        'queue': 'XXX',
+    }
+
+  if not os.path.exists(dictionary['queue']):
+    os.makedirs(dictionary['queue'])
+
+  output_base = ''.join((
+      dictionary['jobname'].replace('$', '_').replace('/', '-'), '.',
+      'listing', '-',
+      dictionary['number'].replace(' ', '0'), '-',
+      dictionary['class'])).replace(' ', '')
+
+  output_base = '/'.join((dictionary['queue'], output_base))
+
+  output_name = output_base
+  for i in range(1, 1000):
+    if not os.path.exists(output_name):
+      break
+    output_name = output_base + '-' + str(i)
+
+  print('Opening output file', output_name, file=sys.stderr)
+  return open(output_name, 'w')
+
+def _CloseFile(file_handle):
+  """Close a file handle if needed."""
+  if file_handle:
+    print('Closing output file', file_handle.name, file=sys.stderr)
+    file_handle.close()
+
 def _Process():
   """Main processing loop.  Never exits until program shutdown."""
-  file_buffer = []
   page_buffer = []
-  separator_page = False
-  eof = False
-  dictionary = {}
+  banner_page = False
   line = True
+  file_handle = None
 
   while line:
     line = _GetLine()
 
-    # Handle EOF when we have data buffered
-    if not line and not dictionary and not eof:
-      if file_buffer or page_buffer:
-        queue = 'XXX'
-        if not os.path.exists(queue):
-          os.makedirs(queue)
+    # At EOF, flush any current page data (if not a banner page)
+    # and always exit
+    if not line:
+      if page_buffer and not banner_page:
 
-        for i in range(1, 1000):
-          output_name = queue + '/' + 'UNKNOWN.listing' + str(i)
-          if not os.path.exists(output_name):
-            break
-        with open(output_name, 'w') as f:
-          f.write(''.join(file_buffer + page_buffer))
+        if not file_handle:
+          file_handle = _OpenFile({})
+        file_handle.write(''.join(page_buffer))
+        _CloseFile(file_handle)
       return
 
+    #  print/flush any previous page when we see top of form
+    if '\f' in line and (page_buffer and page_buffer != ['\n']):
+      if banner_page:
+        # We flush (not print) banner pages
+        banner_page = False
+      else:
+        if not file_handle:
+          # If input did not start with a banner page, we need to
+          # open an anonymous file after the first page
+          print('New file for:\n', '->'.join(page_buffer), file=sys.stderr)
+          file_handle = _OpenFile({})
+        file_handle.write(''.join(page_buffer))
 
-    matches = re.match(_JES2_REGEX, line.strip())
-    if not matches:
-      matches = re.match(_HASP_REGEX, line.strip())
+      # Having printed/flush previous page, start a new one
+      page_buffer = []
 
-    if matches:
-      separator_page = True
-      page_buffer.append(line)
-      dictionary = matches.groupdict()
-      eof = 'END' in dictionary['edge']
-    elif dictionary and eof and (not line or line.startswith('\f')):
-      output_base = ''.join((
-          dictionary['name'].replace('$', '_').replace('/', '-'), '.',
-          'listing', '-',
-          dictionary['number'].replace(' ', '0'), '-',
-          dictionary['class'])).replace(' ', '')
-      output_base = '/'.join((dictionary['queue'], output_base))
+    page_buffer.append(line)
 
-      if not os.path.exists(dictionary['queue']):
-        os.makedirs(dictionary['queue'])
+    if not banner_page:
+      for regex in (_JES2_REGEX, _HASP_REGEX, _WTR_REGEX):
+        matches = re.match(regex, line.strip())
 
-      output_name = output_base
-      for i in range(1, 1000):
-        if not os.path.exists(output_name):
+        # If a match, we have a banner page which may need a new file
+        if matches:
+          banner_page = True
+          dictionary = matches.groupdict()
+          dictionary.setdefault('number', '9999') # WTR files have no number
+          _CloseFile(file_handle)
+
+          # We only open files for START banner pages; at the end,
+          # we ignore it, having already closed the file.
+          if 'END' in dictionary['edge']:
+            file_handle = None
+          else:
+            file_handle = _OpenFile(dictionary)
           break
-        output_name = output_base + '-' + str(i)
-
-      with open(output_name, 'w') as f:
-        f.write(''.join(file_buffer))
-      print(sys.argv[0], 'Wrote', output_name, 'with', len(file_buffer), 'lines', file=sys.stderr)
-      file_buffer = []
-      separator_page = False
-      dictionary = {}
-    else:
-      if line.startswith('\f'):
-        if separator_page:
-          separator_page = False
-        else:
-          file_buffer += page_buffer
-        page_buffer = []
-      dictionary = {}
-      page_buffer.append(line)
-
 
 def Main():
   """Main program to invoke _Process."""
