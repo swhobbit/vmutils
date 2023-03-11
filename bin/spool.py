4264b98a9b433a4b168be53cb4851f1690e15a48
Drew Derbyshire <ahd@kew.com>
2023-03-11 12:53:09 -0800

    Support for printing JOB output w/o banner pages  Please enter the commit message for your changes. Lines starting

--- bin/spool.py
+++ bin/spool.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# vim:  ts=2 sw=2 expandtab
+#         vim:  ts=2 sw=2 expandtab
 
 """Split HASP/JES2 spool output running in MVS or MVT w/HASP under a
 Hercules emulated machine into individual files based on their trailing
@@ -48,7 +48,7 @@ _JES2_PATTERN = (
     r'  [ \d]\d(?:\.\d\d){2,2} (?:A|P)M'    # time: hh:mm:ss AM/PM
     r' \d\d [A-Z]{3,3} \d\d'                # date: dd mmm yy
     r'  ([\w ]{8,8})'                       # printer name
-    r'  SYS (?P<smf>[\w ]{4,4})'            # system name
+    r'  SYS (?P<node>[\w ]{4,4})'           # system SMF name
     r'  (?P=queue)'                         # JOB/TSU/STC
     r' (?P=number)'                         # job number
     r'  (?P=edge)'                          # START/END
@@ -102,28 +102,41 @@ _WTR_PATTERN = (
     r'   '                                  # Literal
     r'(?P<jobname>[A-Z0-9@#$ ]{8,8})'       # job name
     r'   SYS '                              # Literal
-    r'(?P<smf>[ \w]{4,4})'                  # system SMF id
+    r'(?P<node>[ \w]{4,4})'                 # system SMF id
     r'   '                                  # Literal
     r'(?P<queue>WTR|JOB|STC|TSU)'           # WTR JOB/TSU/STC
     r' (?P<printer>[\w ]{3,3})'             # printer address (name)
     r'   '                                  # Literal
-    r'[ \d]\d(:\d\d){2,2} (?:A|P)M'      # time: hh:mm:ss AM/PM
+    r'[ \d]\d(:\d\d){2,2} (?:A|P)M'         # time: hh:mm:ss AM/PM
     r'  '                                   # Literal
     r'[ \d]\d [A-Z]{3,3} \d{4,4}'           # date: dd mmm yyyy
     r'   '                                  # Literal
     r'(?P=queue)'                           # WTR
     r' (?P=printer)'                        # printer address (name)
     r'   SYS '                              # Literal
-    r'(?P=smf)'                             # system SMF id
+    r'(?P=node)'                            # system SMF id
     r'   '                                  # Literal
-    r'(?P=jobname)'                        # job name
+    r'(?P=jobname)'                         # job name
     r'   '                                  # Literal
-    r'(?P=edge)'                           # START/END
+    r'(?P=edge)'                            # START/END
     r'  '                                   # Literal
-    r'(?P=class)'                          # sysout class
+    r'(?P=class)'                           # sysout class
     r'\*{11,11}'                            # Literal
 )
 _WTR_REGEX = re.compile(_WTR_PATTERN)
+
+# pylint: disable=C0301
+#       ....+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8....+....9....+....*....+....1....+....2....+....3..#
+#       //INIT     JOB MSGLEVEL=1
+#       ....+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8....+....9....+....*....+....1....+....2....+....3..
+
+_NOBANNER_PATTERN = (
+    r'(//|IEF452I )(?P<jobname>[A-Z0-9@#$]+)' # jobname or failure of some job
+    r' +JOB '                                # it's a JOB statement
+#   r'.*(MSGCLASS=(?P<class>[A-Z0-9]))?'     # which MAYBE has a class
+    r'.*'                                    # Unparsed trailing
+)
+_NOBANNER_REGEX = re.compile(_NOBANNER_PATTERN)
 
 def _GetLine():
   """Read one line of sysout from the input"""
@@ -144,27 +157,33 @@ def _GetLine():
     if c in ('\n', '\f'):
       return line
 
-def _OpenFile(dictionary):
+def _OpenFile(dictionary, sequence, lines_in):
   """Open a new spool based on provided job information."""
   if not dictionary:
-    # Fake job information if none provided
+    # Fake job information since none provided
     current = datetime.now()
-    dictionary = {
-        'class': 'Q',
-        'jobname': ''.join(('X',
-                            current.strftime('%j%H%M'))),
-        'number': current.strftime('%S'),
-        'smf': 'SPL',
-        'queue': 'XXX',
-    }
+    dictionary['jobname'] = 'X' + current.strftime('%j%H%M')
+
+  for (key, values) in (('node', (os.getenv('HERCULES_NAME'), 'nonet')),
+                        ('class', ('Y',)),
+                        ('queue', ('noqueue',)),
+                        ('number', (str(sequence),))):
+    for value in values:
+      if value and not key in dictionary:
+        dictionary[key] = value
 
   if not os.path.exists(dictionary['queue']):
     os.makedirs(dictionary['queue'])
 
   output_base = ''.join((
-      dictionary['jobname'].replace('$', '_').replace('/', '-'), '.',
-      'listing', '-',
-      dictionary['number'].replace(' ', '0'), '-',
+      dictionary['jobname'].replace('$', '_').replace('/', '-'),
+      '.',
+      'listing',
+      '-',
+      dictionary['node'],
+      '-',
+      dictionary['number'].replace(' ', '0'),
+      '-',
       dictionary['class'])).replace(' ', '')
 
   output_base = '/'.join((dictionary['queue'], output_base))
@@ -175,35 +194,72 @@ def _OpenFile(dictionary):
       break
     output_name = output_base + '-' + str(i)
 
-  print('Opening output file', output_name, file=sys.stderr)
+  print('Opening output file', output_name,
+        'after', lines_in or 'no', 'lines read',
+         file=sys.stderr)
   return open(output_name, 'w')
 
-def _CloseFile(file_handle):
+def _CloseFile(file_handle, lines_out, lines_in):
   """Close a file handle if needed."""
   if file_handle:
-    print('Closing output file', file_handle.name, file=sys.stderr)
+    print('Closing output file with',
+          lines_out or "no", 'lines written (input had',
+          lines_in or "no", "total lines in)",
+          file=sys.stderr)
     file_handle.close()
+
+def _ScanForBanner(line, new_page, last_regex):
+  """Scan current line for a banner text."""
+  if not line.strip():
+    return (None, last_regex)
+
+  regex_list = [_JES2_REGEX, _HASP_REGEX, _WTR_REGEX]
+
+  # Only search for the raw JCL starting a job if starting a page AND
+  # we not yet seen a real banner page.
+  if new_page and last_regex is _NOBANNER_REGEX:
+    regex_list.append(last_regex)
+
+  for regex in regex_list:
+    matches = re.match(regex, line.strip())
+    if matches:
+      dictionary = matches.groupdict()
+      return (dictionary, regex)
+
+  return (None, last_regex)
+
 
 def _Process():
   """Main processing loop.  Never exits until program shutdown."""
   page_buffer = []
   banner_page = False
-  line = True
+  new_page = False
+  form_feed = False
   file_handle = None
+  lines_out = 0
+  lines_in = 0
+  line = True
+
+  last_regex = _NOBANNER_REGEX
+  sequence = 10000
 
   while line:
     line = _GetLine()
+    new_page = form_feed
+    form_feed = '\f' in line
 
-    # At EOF, flush any current page data (if not a banner page)
-    # and always exit
+    # At EOF, flush any current page (unless a banner page) and exit
     if not line:
       if page_buffer and not banner_page:
-
         if not file_handle:
-          file_handle = _OpenFile({})
+          file_handle = _OpenFile({}, sequence + 1, lines_in)
         file_handle.write(''.join(page_buffer))
-        _CloseFile(file_handle)
+        _CloseFile(file_handle, lines_out, lines_in)
+        lines_out = 0
       return
+
+    lines_in += 1
+    lines_out += 1
 
     #  print/flush any previous page when we see top of form
     if '\f' in line and (page_buffer and page_buffer != ['\n']):
@@ -213,33 +269,35 @@ def _Process():
       else:
         if not file_handle:
           # If input did not start with a banner page, we need to
-          # open an anonymous file after the first page
+          # open an anonymous file now that we have the first page
           print('New file for:\n', '->'.join(page_buffer), file=sys.stderr)
-          file_handle = _OpenFile({})
+          sequence = sequence + 1
+          file_handle = _OpenFile({}, sequence, lines_in)
+
         file_handle.write(''.join(page_buffer))
 
-      # Having printed/flush previous page, start a new one
+      # Having printed/flushed the previous page, start a new one
       page_buffer = []
 
     page_buffer.append(line)
 
+    # Look for a banner which starts end a new file.
     if not banner_page:
-      for regex in (_JES2_REGEX, _HASP_REGEX, _WTR_REGEX):
-        matches = re.match(regex, line.strip())
+      (dictionary, last_regex) = _ScanForBanner(line, new_page, last_regex)
 
+      if dictionary:
         # If a match, we have a banner page which may need a new file
-        if matches:
-          banner_page = True
-          dictionary = matches.groupdict()
-          dictionary.setdefault('number', '9999') # WTR files have no number
-          _CloseFile(file_handle)
-          file_handle = None
+        banner_page = 'edge' in dictionary
+        _CloseFile(file_handle, lines_out, lines_in)
+        lines_out = 0
+        file_handle = None
 
-          # We only open files for START banner pages; at the end,
-          # we ignore it, having already closed the file.
-          if 'END' not in dictionary['edge']:
-            file_handle = _OpenFile(dictionary)
-          break
+        # We only open files for START banner pages; at the end of jobs,
+        # we ignore it, having already closed the file.
+        if not banner_page or 'END' not in dictionary['edge']:
+          sequence += 1
+          file_handle = _OpenFile(dictionary, sequence, lines_in)
+          dictionary = None
 
 def Main():
   """Main program to invoke _Process."""
@@ -250,7 +308,7 @@ def Main():
     os.chdir('prt')
   print('Current directory now', os.getcwd(), file=sys.stderr)
   _Process()
-  print(sys.argv[0], 'EOF!!!', file=sys.stderr)
+  print(sys.argv[0], '\nEOF!\n', file=sys.stderr)
 
 if __name__ == '__main__':
   sys.exit(Main())
